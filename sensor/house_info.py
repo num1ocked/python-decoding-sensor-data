14417bfc24e588f3a13f2c95fa8909c8a138a085
num1ocked <105787413+num1ocked@users.noreply.github.com>
2022-05-23 10:14:05 +0100

    2

--- sensor/house_info.py
+++ sensor/house_info.py
@@ -2,5 +2,5 @@ class HouseInfo(object):
     def __init__(self, data):
         self.data = data
 
-    def get_data_by_area(self, field, rec_area):
+    def get_data_by_area(self, field, rec_area=0):
         def field_data = []
