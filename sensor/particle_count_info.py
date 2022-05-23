f8ed04ff234e4bd07e98b11ff8b78c0a814f3fce
num1ocked <105787413+num1ocked@users.noreply.github.com>
2022-05-23 16:09:00 +0100

    9

--- sensor/particle_count_info.py
+++ sensor/particle_count_info.py
@@ -1,4 +1,5 @@
 from house_info import HouseInfo
+from datetime import date, datetime
 
 class ParticleData(HouseInfo):
     def _convert_data(self, data):
@@ -6,3 +7,11 @@ class ParticleData(HouseInfo):
         for rec in data:
             recs.append(float(rec))
         return recs
+
+    def get_data_by_area(self, rec_area=0):
+        recs = super().get_data_by_area("particulate", rec_area)
+        return self._convert_data(recs)
+
+    def get_data_by_date(self, rec_date=date.today()):
+        recs = super().get_data_by_date("particulate", rec_date)
+        return self._convert_data(recs)
