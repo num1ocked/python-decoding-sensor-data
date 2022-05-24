60f978ece64a01c4b5a481f35567c8bd1f7bc16e
num1ocked <105787413+num1ocked@users.noreply.github.com>
2022-05-24 15:08:50 +0100

    3

--- sensor/energy_info.py
+++ sensor/energy_info.py
@@ -9,3 +9,9 @@ class EnergyData(HouseInfo):
         energy = energy & self.ENERGY_BITS
         energy = energy >> 4
         return energy
+
+    def _convert_data(self, data):
+        recs = []
+        for rec in data:
+            recs.append(self._get_energy(rec))
+        return recs
